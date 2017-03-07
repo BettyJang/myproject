48893ea81d01e289f14fb892a051a251e5564764
BettyJang <bettyjang1005@gmail.com>
2017-03-07 16:25:24 +0800

    The progress of abstraction

---
+++ src/oo/abstraction/Car.java
@@ -0,0 +1,15 @@
+package oo.abstraction;
+
+public class Car {
+      String brand;
+      String name;
+      int cc;
+      String type;
+      public Car(String brand,String name,int cc,String type){
+    	  this.brand = brand;
+    	  this.name = name;
+    	  this.cc = cc;
+    	  this.type = type;
+      }
+      
+}
