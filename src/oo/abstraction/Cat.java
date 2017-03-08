36209a097f9e8e47497f45c15d0aa6a41f8e7941
BettyJang <bettyjang1005@gmaile.com>
2017-03-09 00:21:11 +0800

    The progress of abstraction - homework2

--- src/oo/abstraction/Cat.java
+++ src/oo/abstraction/Cat.java
@@ -1,5 +1,18 @@
 package oo.abstraction;
 
 public class Cat {
+       String name;
+       String varity;
+       String gender;
+       int ages;
+       String birthplace;
+       public Cat(String name,String varity,String gender,int ages,String birthplace){
+    	   this.name = name;
+    	   this.varity = varity;
+    	   this.gender = gender;
+    	   this.ages = ages;
+    	   this.birthplace = birthplace;
+       }
+    	   
 
 }
--- src/oo/abstraction/CatTester.java
+++ src/oo/abstraction/CatTester.java
@@ -3,7 +3,12 @@ package oo.abstraction;
 public class CatTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cat cat1 = new Cat("Carol","Mix","Female",2,"Taiwan");
+		Cat cat2 = new Cat("Aaron","American Shorthair","Male",5,"American");
+		Cat cat3 = new Cat("Sally","British Longhair","Female",3,"Britian");
+		Cat cat4 = new Cat("Frank","Mix","Male",10,"Taiwan");
+		Cat cat5 = new Cat("Dorothy","Mix","Female",12,"Taiwan");
+		Cat cat6 = new Cat("King","Maine Cat","Male",1,"American");
 
 	}
 
