7a615cac2f623d48251ba2a3b124dc2ea5029ee4
user <user@es302n15>
2017-03-14 15:56:57 +0800

    新增建構子(Constructor)

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -4,12 +4,13 @@ public class Car {
       String brand;
       String name;
       int cc;
-      String type;
-      public Car(String brand,String name,int cc,String type){
+      //String type;
+      //int status;
+      public Car(String brand,String name,int cc){
     	  this.brand = brand;
     	  this.name = name;
     	  this.cc = cc;
-    	  this.type = type;
+    	  //this.type = type;
       }
       
 }
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,7 +3,12 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota" , "Altis" , 1800 , "Sedan");
+		Car c1 = new Car("Toyota","Alits",1800);
+		
+		Car c2 = new Car("Honda","Accord",2000);
+		Car c3 = new Car("Toyota","Wish",2000);
+		
 	}
 
 }
+
