e924e45e8f0be1f761f5940e59aab0ffc02e68cb
user <user@es302n15>
2017-03-21 16:23:40 +0800

    Fileds,Car id,Arrary

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,12 +3,43 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota","Alits",1800);
-		
+		Car c1 = new Car("Toyota","Altis",1800);	
 		Car c2 = new Car("Honda","Accord",2000);
 		Car c3 = new Car("Toyota","Wish",2000);
+		//Arrays in Java
+		int [] n = new int[5];
+		//n[0] = 0;
+		//n[1] = 0;
+		//n[2] = 0;
+		//n[3] = 0;
+		System.out.println(n[2]);
 		
+		Car[] cars = new Car[3];
+		
+		System.out.println(cars[0]);
+		cars[0] = new Car("Toyota","Altis",1800);
+		cars[1] = new Car("Honda","Accord",2000);
+		cars[2] = new Car("Toyota","Wish",2000);
+		cars[0].id = 301;
+		cars[1].id = 400;
+		cars[2].id = 403;
+		System.out.println(cars[0].name);
+		int i = 0;
+		for (i=0;i<3;i++){
+			System.out.println(cars[i].name);
+		}
+		System.out.println(i);
+		int j = 1;
+		for(j=1;j<10;j=j+2){
+			System.out.println(j);
+		}
+		System.out.println(j);
+		class AA{	
+		}
+		
+		AA a = new AA();
+		}
 	}
 
-}
+
 
