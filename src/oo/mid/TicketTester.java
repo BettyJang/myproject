d404a005bae0dc7c7bb412b2fe1b8e2af9c5436f
BettyJang <bettyjang1005@gmail.com>
2017-04-25 14:29:12 +0800

    mid 0425

---
+++ src/oo/mid/TicketTester.java
@@ -0,0 +1,24 @@
+package oo.mid;
+
+import java.util.ArrayList;
+
+public class TicketTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ArrayList<String> a = new ArrayList<>();
+        a.add("Beauty and the Beast");
+        a.add("Baby Boss");
+        a.add("目擊者");
+        a.add("玩命關頭8");
+        a.add("逃出絕命鎮");
+        String a1 = a.get(0);
+        String a2 = a.get(1);
+        String a3 = a.get(2);
+        String a4 = a.get(3);
+        String a5 = a.get(4);
+        int [] n = new int[5];
+        
+	}
+
+}
