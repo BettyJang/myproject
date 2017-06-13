73af6922a460ec6316fd5f695dc813e6abecb990
BettyJang <bettyjang1005@gmail.com>
2017-06-13 16:13:48 +0800

    0613 Why implement hide? & 繼承建構子使用

--- src/oo/test/Member.java
+++ src/oo/test/Member.java
@@ -4,8 +4,13 @@ public class Member {
      String id;
     private int age;
      String name;
-     public Member(){
+     public Member(String name){//父類別的建構子中加入參數 而子類別卻沒有在建構子
+    	                        //中加入super()的話 便無法執行
     	 System.out.println("member construction");
+    	 this.name = name;
+     }
+     public Member(){
+    	 
      }
      public void setAge(int age){
     	 if(age<0){
--- src/oo/test/MemberTester.java
+++ src/oo/test/MemberTester.java
@@ -4,11 +4,12 @@ public class MemberTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Member member = new Member();
+       Member member = new Member(null);//因為父類別中建構子加入參數 所以要在此括號中加入String的職
        member.setAge(-5);
-       System.out.print(member.getAge());
+       System.out.println(member.getAge());
        //PhoneMember
        PhoneMember member2 = new PhoneMember();
+       PhoneMember member3 = new PhoneMember("Hank");
 	}
 	
 
