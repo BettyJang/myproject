b6a34ae1187d2fa5a06afe1390cbe31cff27e1ae
BettyJang <bettyjang1005@gmail.com>
2017-06-13 15:52:04 +0800

    0613 Why implement hide? & 建構子

---
+++ src/oo/test/MemberTester.java
@@ -0,0 +1,15 @@
+package oo.test;
+
+public class MemberTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Member member = new Member();
+       member.setAge(-5);
+       System.out.print(member.getAge());
+       //PhoneMember
+       PhoneMember member2 = new PhoneMember();
+	}
+	
+
+}
